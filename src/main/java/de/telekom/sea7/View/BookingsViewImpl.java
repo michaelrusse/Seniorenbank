59266b7e362dd2973090c96d648df8cfdde53ed6
Robert Esser <robert-esser_student2021@wilder-school>
2022-01-28 12:49:33 +0100

    Update Import

--- src/main/java/de/telekom/sea7/View/BookingsViewImpl.java
+++ src/main/java/de/telekom/sea7/View/BookingsViewImpl.java
@@ -178,7 +178,7 @@ public class BookingsViewImpl implements BookingsView {
 
 	private void csv1reader() {
 
-		// Einlesen des Files und spliten
+		// Einlesen des Files und splitten
 		FileReader myFile = null;
 		BufferedReader buff = null;
 		final List<String> lines = new ArrayList<String>();
@@ -186,10 +186,21 @@ public class BookingsViewImpl implements BookingsView {
 		try {
 			myFile = new FileReader("test.csv");
 			buff = new BufferedReader(myFile);
-			String line;
+			String line = "";
+			String trenner = ",";
 			buff.readLine();
 			while ((line = buff.readLine()) != null) {
 				// System.out.println(line); // kontrolle was eingelesen
+				String[] ausgelesenerWert = line.split(trenner);
+				float betrag = Float.parseFloat(ausgelesenerWert[0]);
+				String empfaenger = ausgelesenerWert[1];
+				String iban = ausgelesenerWert[2];
+				String bic = ausgelesenerWert[3];
+				String verwendungszweck = ausgelesenerWert[4];
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+				LocalDateTime datum = LocalDateTime.parse(ausgelesenerWert[5]);
+				Booking bookingimpl = new BookingImpl(betrag, empfaenger, iban, bic, verwendungszweck, datum);
+				bookingsimpl.add(bookingimpl);
 
 				lines.add(line);
 			}
@@ -203,50 +214,54 @@ public class BookingsViewImpl implements BookingsView {
 				System.err.println("Error2 :" + e);
 			}
 		}
-		
 
-		final String[][] valuesArray = new String[lines.size()][];
-		int cnt = 0;
-		for (final String line : lines) {
-			valuesArray[cnt++] = line.split(",");
-		}
-
-				
-		
-		// Ausgabe des Array
-		for (String[] arr : valuesArray) {
-			
-			float betrag = Float.parseFloat(arr[0]) ;
-			String empfaenger = arr [1];
-			String iban = arr [2];
-			String bic = arr [3];
-			String verwendungszweck = arr [4];
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-			LocalDateTime datum = LocalDateTime.parse(arr[5]);
-			
-			Booking bookingimpl = new BookingImpl(betrag, empfaenger, iban, bic, verwendungszweck, datum);
-			bookingsimpl.add(bookingimpl); 
-		}
 	}
-	/*
-	 * // Einlesen des Files und spliten FileReader myFile= null; BufferedReader
-	 * buff= null; ArrayList<String> values = new ArrayList<String>(); String []
-	 * valuesSplited;
-	 * 
-	 * try { myFile =new FileReader("test.csv"); buff =new BufferedReader(myFile);
-	 * while (true) { String line = buff.readLine(); if (line == null) break;
-	 * values.add(line); System.out.println (line); //kontrolle was eingelesen
-	 * valuesSplited = line.split(","); // Spliten nach dem Sonderzeichen "," } }
-	 * catch (IOException e) { System.err.println("Error2 :"+e); }finally { try{
-	 * buff.close(); myFile.close(); }catch (IOException e) {
-	 * System.err.println("Error2 :"+e); }
-	 * 
-	 * // Umwandeln in Array Object valuesArray[][]= valuesSplited.toArray();
-	 * //Umwandeln in Array //Ausgabe des Array for ( int i= 0 ; i <
-	 * valuesArray.length; i++ ) {for (int j=0; j< valuesArray[i].length; ++j) {
-	 * System.out.print(valuesArray[i][j]); }}
-	 * 
-	 * 
-	 * }
-	 */
 }
+
+/*
+ * private void csv1reader() {
+ * 
+ * // Einlesen des Files und splitten. Erstellung von 2 neuen Objektinstanzen //
+ * der Klasse Filereader und BufferedReader. Setzen der Instanzen auf NULL.
+ * //Erstelung einer finalen Liste als Arraylist. Frage: Warum final ?
+ * 
+ * FileReader myFile = null; BufferedReader buff = null; final List<String>
+ * lines = new ArrayList<String>();
+ * 
+ * // im try-catch: Zuweisung test.csv, Anlegen einer neuen String-Instanz mit
+ * dem Namen "line". //Anlegen der Variablen buff -> Erhält zeilenweise den
+ * Inhalt von myFile über den BufferedReader. //Anlegen der Stringvar line
+ * //buff.readline bewirkt einen Skip der ersten Zeile der CSV try { myFile =
+ * new FileReader("test.csv"); buff = new BufferedReader(myFile); String line;
+ * buff.readLine(); //Schleife wird so lange durchlaufen bis der eigelesene Wert
+ * null ist. //Danach wird die jeweilige Zeile in das Array eingelesen
+ * 
+ * 
+ * while ((line = buff.readLine()) != null) { // System.out.println(line); //
+ * kontrolle was eingelesen
+ * 
+ * lines.add(line);
+ * 
+ * //Exception-catch IO Exception mit Ausgabe Fehlermeldung // finally - Bereich
+ * mit Schliessung der "Arbeitsdateien im //try-catch mit Fehlermeldung } }
+ * catch (IOException e) { System.err.println("Error2 :" + e); } finally { try {
+ * buff.close(); myFile.close(); } catch (IOException e) {
+ * System.err.println("Error2 :" + e); } }
+ * 
+ * 
+ * final String[][] valuesArray = new String[lines.size()][]; int cnt = 0; for
+ * (final String line : lines) { valuesArray[cnt++] = line.split(","); }
+ * 
+ * 
+ * 
+ * // Ausgabe des Array for (String[] arr : valuesArray) {
+ * 
+ * float betrag = Float.parseFloat(arr[0]) ; String empfaenger = arr [1]; String
+ * iban = arr [2]; String bic = arr [3]; String verwendungszweck = arr [4];
+ * DateTimeFormatter formatter =
+ * DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"); LocalDateTime datum =
+ * LocalDateTime.parse(arr[5]);
+ * 
+ * Booking bookingimpl = new BookingImpl(betrag, empfaenger, iban, bic,
+ * verwendungszweck, datum); bookingsimpl.add(bookingimpl); } }
+ */
